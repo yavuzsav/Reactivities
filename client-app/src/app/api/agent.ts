bb34ae502c027683e6a2fb5228c78fd57bf1fa4f
Yavuz SAV <yavuzsav07@gmail.com>
2020-02-22 13:30:44 +0300

    End of Section 18

--- client-app/src/app/api/agent.ts
+++ client-app/src/app/api/agent.ts
@@ -72,7 +72,8 @@ const Profiles = {
   get: (userName: string): Promise<IProfile> => requests.get(`/profiles/${userName}`),
   uploadPhoto: (photo: Blob): Promise<IPhoto> => requests.postForm("/photos", photo),
   setMainPhoto: (id: string) => requests.post(`/photos/${id}/setMain`, {}),
-  deletePhoto: (id: string) => requests.del(`/photos/${id}`)
+  deletePhoto: (id: string) => requests.del(`/photos/${id}`),
+  updateProfile: (profile: Partial<IProfile>) => requests.put(`/profiles`, profile)
 }
 
 export default {
--- client-app/src/app/models/profile.ts
+++ client-app/src/app/models/profile.ts
@@ -1,7 +1,7 @@
 export interface IProfile {
   displayName: string;
   userName: string;
-  boi: string;
+  bio: string;
   image: string;
   photos: IPhoto[];
 }
